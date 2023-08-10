0694b293bffc3b8c9be0a73602941ec80681f774
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-08-11 00:24:16 +0200

    refact postres

--- main.js
+++ main.js
@@ -17,8 +17,6 @@ let navbar = document.querySelector('.header .navbar');
 menuBtn.onclick = () =>{
    menuBtn.classList.toggle('fa-times');
    navbar.classList.toggle('active');
-   searchBtn.classList.remove('fa-times');
-   searchForm.classList.remove('active');
 }
 
 document.addEventListener("DOMContentLoaded", () => {
