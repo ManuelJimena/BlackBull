ccecbd5be567b4fe13615f1d37899f20a343f883
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-08-11 00:44:41 +0200

    refact postres

--- components/BurguerCard/BurguerCard.js
+++
@@ -1,9 +0,0 @@
-import "./BurguerCard.css";
-export const BurguerCard = (burguer) => `
-<div class="portfolio-box">
-      <img src=${burguer.image} alt=${burguer.description}/>
-    <div class="portfolio-layer">
-      <h4>${burguer.description}</h4>
-    </div>
-</div>
-`;
